6908aa14e8ee675877ec7a4352b64fd2266615a0
Johnatan Coelho <johnatansouza242@gmail.com>
2025-04-18 21:55:30 -0300

    ✨ feat: Modelo para telas

--- Aplication/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.horarioaula {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.horarioaula to javafx.fxml;
-    exports com.example.horarioaula;
-}
