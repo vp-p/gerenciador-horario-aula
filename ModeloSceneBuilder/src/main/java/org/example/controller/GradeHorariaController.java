ea5b0a9ed0c5a7a3a3ec72baa2077ea0a19f8876
Johnatan Coelho <johnatansouza242@gmail.com>
2025-04-18 21:51:35 -0300

    ModeloSceneBuilder

---
+++ ModeloSceneBuilder/src/main/java/org/example/controller/GradeHorariaController.java
@@ -0,0 +1,35 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GradeHorariaController {
+
+    @FXML
+    private Button btnAdicionarAula;
+
+    @FXML
+    private Button btnCoordenador;
+
+    @FXML
+    private Button btnCursos;
+
+    @FXML
+    private Button btnDeletar;
+
+    @FXML
+    private Button btnDisciplinas;
+
+    @FXML
+    private Button btnFiltro;
+
+    @FXML
+    private Button btnGradeHoraria;
+
+    @FXML
+    private Button btnProfessor;
+
+    @FXML
+    private Button btnSemestres;
+
+}
